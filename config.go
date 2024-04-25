ecf591105f356b9c3c426952548e036c0a19ad75
Dan Lyu <dan@lyu.sh>
2024-04-25 11:33:47 -0400

    Update config

--- config.go
+++ config.go
@@ -19,17 +19,17 @@ type Config struct {
 	Ffmpeg              string `env:"FFMPEG" envDefault:"ffmpeg"`
 	Ffprobe             string `env:"FFPROBE" envDefault:"ffprobe"`
 	Opusenc             string `env:"OPUSENC" envDefault:"opusenc"`
-	HandbrakeCli        string `env:"HANDBRAKE_CLI" envDefault:"./HandBrakeCLI"`
+	HandbrakeCli        string `env:"HANDBRAKE_CLI" envDefault:"HandBrakeCLI"`
 	Av1Preset           string `env:"AV1_PRESET" envDefault:"6"`
 	NvencPreset         string `env:"NVENC_PRESET" envDefault:"slower"`
-	ConstantQuality     string `env:"CONSTANT_QUALITY" envDefault:"20"`
+	ConstantQuality     string `env:"CONSTANT_QUALITY" envDefault:"22"`
 	SubtitleExt         string `env:"SUBTITLE_EXT" envDefault:".vtt"`
 	VideoExt            string `env:"VIDEO_EXT" envDefault:"mp4"`
 	SkipAudioExtraction bool   `env:"SKIP_AUDIO_EXTRACTION" envDefault:"true"`
 	DiscordUserName     string `env:"DISCORD_USER_NAME" envDefault:"Sparkle"`
 	DiscordWebhook      string `env:"DISCORD_WEBHOOK" envDefault:""`
 	Host                string `env:"HOST" envDefault:"http://localhost"`
-	Encoder             string `env:"ENCODER" envDefault:"av1,hevc"`
+	Encoder             string `env:"ENCODER" envDefault:"av1"`
 	Av1Encoder          string `env:"SVT_AV1_ENCODER" envDefault:"svt_av1_10bit"`
 	HevcEncoder         string `env:"HEVC_ENCODER" envDefault:"nvenc_h265_10bit"`
 }
