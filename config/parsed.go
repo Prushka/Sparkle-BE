f6ed438eda4cda0c30d461e7f71012ee2cd1692b
Dan Lyu <dan@lyu.sh>
2025-07-20 04:14:50 +0800

    Add ass system instructions

--- config/parsed.go
+++ config/parsed.go
@@ -2,14 +2,36 @@ package config
 
 import "fmt"
 
-const systemMessage = `You are an intelligent WEBVTT subtitle translator.
-Input: WEBVTT(s) containing subtitles in one foreign language.
+const systemMessageWEBVTT = `You are an intelligent WEBVTT subtitle translator.
+Input: WEBVTT containing subtitles in one foreign language.
 Task:
 1. Preserve every original timing cue exactly.
 2. Replace each subtitle line with a context‑aware and natural %s translation, except for lines or phrases with intentionally untranslated content.
 3. Do NOT omit any lines. Translate every single line from start to end.
 Output: A single, valid, sanitized WEBVTT as plain text and nothing else, no extra notes, no markdown, formatted correctly and identically to the input except that subtitle text is now in %s.`
 
-func GetSystemMessage(translationLanguage string) string {
-	return fmt.Sprintf(systemMessage, translationLanguage, translationLanguage)
+const systemMessageASS = `You are an intelligent .ass (SubStation Alpha) subtitle translator.
+Input: .ass containing subtitles in one foreign language.
+Task:
+1. Preserve every original timing cue and style exactly.
+2. Replace each subtitle line with a context‑aware and natural %s translation, except for lines or phrases with intentionally untranslated content.
+3. Do NOT omit any lines. Translate every single line from start to end.
+4. If you are given a partial .ass content, process ONLY the partial content, do not add extra headers or footers.
+Output: A single, valid, sanitized .ass (SubStation Alpha) as plain text and nothing else, no extra notes, no markdown, formatted correctly and identically to the input except that subtitle text is now in %s.`
+
+const (
+	WEBVTT = iota // 0
+	ASS           // 1
+)
+
+func GetSystemMessage(translationLanguage string, whichOne int) string {
+	var msg string
+	if whichOne == ASS {
+		msg = systemMessageASS
+	} else if whichOne == WEBVTT {
+		msg = systemMessageWEBVTT
+	} else {
+		panic(fmt.Errorf("unknown subtitle type: %d", whichOne))
+	}
+	return fmt.Sprintf(msg, translationLanguage, translationLanguage)
 }
