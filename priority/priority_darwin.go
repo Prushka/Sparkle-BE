6485324f1851d4a2e582071dc6ade9907775fe5e
Dan Lyu <dan@lyu.sh>
2025-07-24 14:28:56 +0800

    Add priority darwin and linux

--- priority/priority_darwin.go
+++ priority/priority_darwin.go
@@ -2,6 +2,26 @@
 
 package priority
 
+import (
+	"Sparkle/discord"
+	"syscall"
+)
+
+// Priority constants for macOS
+const (
+	PRIO_PROCESS = 0  // Process priority type
+	LOW_PRIORITY = 16 // Nice value for low priority (higher nice = lower priority)
+)
+
+func setPriorityDarwin(pid int, priority int) error {
+	return syscall.Setpriority(PRIO_PROCESS, pid, priority)
+}
+
 func LowPriority(pid int) error {
+	err := setPriorityDarwin(pid, LOW_PRIORITY)
+	if err != nil {
+		discord.Errorf("error setting low priority for pid %d: %v", pid, err)
+		return err
+	}
 	return nil
 }
--- priority/priority_linux.go
+++ priority/priority_linux.go
@@ -1,7 +1,27 @@
-//go:build linux || darwin
+//go:build linux
 
 package priority
 
+import (
+	"Sparkle/discord"
+	"syscall"
+)
+
+// Priority constants for Linux
+const (
+	PRIO_PROCESS = 0  // Process priority type
+	LOW_PRIORITY = 16 // Nice value for low priority (higher nice = lower priority)
+)
+
+func setPriorityLinux(pid int, priority int) error {
+	return syscall.Setpriority(PRIO_PROCESS, pid, priority)
+}
+
 func LowPriority(pid int) error {
+	err := setPriorityLinux(pid, LOW_PRIORITY)
+	if err != nil {
+		discord.Errorf("error setting low priority for pid %d: %v", pid, err)
+		return err
+	}
 	return nil
 }
