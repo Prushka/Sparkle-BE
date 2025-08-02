6b197d310639b344088d41dac463106bf7cd7ef7
Dan Lyu <dan@lyu.sh>
2025-08-03 03:42:32 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 docker buildx build --platform linux/amd64 \
  --build-arg GIT_COMMIT=$(git rev-list -1 HEAD) \
  --build-arg GIT_VERSION=$(git describe --tags --dirty --always) \
- -f Dockerfile.api --tag meinya/sparkle-api:latest --push .
+ -f Dockerfile.api --tag meinya/sparkle-api:$(git describe --tags --dirty --always) --tag meinya/sparkle-api:latest --push .
 
 
 docker buildx build --platform linux/amd64 \
  --build-arg GIT_COMMIT=$(git rev-list -1 HEAD) \
  --build-arg GIT_VERSION=$(git describe --tags --dirty --always) \
- -f Dockerfile.subtitles --tag meinya/sparkle-subtitles:latest --push .
+ -f Dockerfile.subtitles --tag meinya/sparkle-subtitles:$(git describe --tags --dirty --always) --tag meinya/sparkle-subtitles:latest --push .
+
+echo $(git describe --tags --dirty --always)
