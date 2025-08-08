98e60a89ec0a4c7436018cc02bf00ead9a5d7cf0
Dan Lyu <dan@lyu.sh>
2025-08-09 01:08:44 +0800

    Update translatable text heuristic

--- translation/sanitizeass.go
+++ translation/sanitizeass.go
@@ -263,7 +263,7 @@ func isTranslatableText(dialogueLine string, pos FormatPositions) bool {
 		return false
 	}
 
-	if weakCount >= 2 && wordCount > 0 && blockCount > int(float64(wordCount)*2.5) {
+	if weakCount >= 2 && wordCount > 0 && blockCount > int(float64(wordCount)*3) {
 		return false
 	}
 
