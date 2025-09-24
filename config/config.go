ccba695e90111e859d0adb87be505d52307ba468
Prushka <dan@lyu.sh>
2025-09-25 06:46:37 +0800

    Add russian type

--- config/config.go
+++ config/config.go
@@ -63,7 +63,7 @@ type Config struct {
 	OpenAIModel              string   `env:"OPENAI_MODEL" envDefault:"gpt-5"`
 	OpenAIUrl                string   `env:"OPENAI_URL" envDefault:""`
 	GeminiModel              string   `env:"GEMINI_MODEL" envDefault:"gemini-2.5-pro"`
-	TranslationLanguages     []string `env:"TRANSLATION_LANGUAGES" envDefault:"SIMPLIFIED Chinese/chi,Turkish/tur"` // Turkish/tur,Spanish/spa
+	TranslationLanguages     []string `env:"TRANSLATION_LANGUAGES" envDefault:"SIMPLIFIED Chinese/chi,Turkish/tur"` // Turkish/tur,Spanish/spa,Russian/rus
 	TranslationSubtitleTypes []string `env:"TRANSLATION_SUBTITLE_TYPES" envDefault:"ass"`
 	TranslationBatchLength   int      `env:"TRANSLATION_BATCH_LENGTH" envDefault:"36000"`
 	TranslationAttempts      int      `env:"TRANSLATION_ATTEMPTS" envDefault:"3"`
