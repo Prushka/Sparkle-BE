908a2a1abf835d2cf07c6dc91f6ff27684cfa797
Prushka <dan@lyu.sh>
2025-09-26 19:47:30 +0800

    Fix unavailable delay

--- ai/gemini.go
+++ ai/gemini.go
@@ -102,7 +102,7 @@ func (g *gemini) Send(ctx context.Context, input string) (Result, error) {
 		}
 		if strings.Contains(err.Error(), "try again later") {
 			discord.Errorf("Gemini unavaialble, sleeping..., %v", err)
-			time.Sleep(15 * time.Minute)
+			time.Sleep(7 * time.Minute)
 		}
 		return result, err
 	}
