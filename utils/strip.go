be136b165bcf2d302fe7a142dad721c554856616
Prushka <dan@lyu.sh>
2025-09-26 20:21:04 +0800

    Simplify KeepOnlySubtitles

--- utils/strip.go
+++ utils/strip.go
@@ -10,13 +10,7 @@ const ASSTimeFormat = "15:04:05.00"
 // KeepOnlySubtitles removes the first <think>...</think> tag and its content from the input string only if the string's prefix is <think>
 // it also removes anything that's not a subtitle line
 func KeepOnlySubtitles(input string) string {
-	input = StringArrayOpOnString(input, RemoveEmptyLinesAndTrimSpaces)
-	if strings.HasPrefix(input, "<think>") {
-		endTag := "</think>"
-		endIndex := strings.Index(input, endTag)
-		input = input[endIndex+len(endTag):]
-	}
-	inputLines := strings.Split(input, "\n")
+	inputLines := RemoveEmptyLinesAndTrimSpaces(strings.Split(input, "\n"))
 	var outputLines []string
 	for _, line := range inputLines {
 		if HasTimePrefix(line) {
@@ -42,12 +36,6 @@ func HasTimePrefix(s string) bool {
 
 	// If there is no error, the prefix is a valid time in the given format.
 	return err == nil
-}
-
-func StringArrayOpOnString(input string, op func([]string) []string) string {
-	lines := strings.Split(input, "\n")
-	processedLines := op(lines)
-	return strings.Join(processedLines, "\n")
 }
 
 func RemoveEmptyLinesAndTrimSpaces(block []string) []string {
