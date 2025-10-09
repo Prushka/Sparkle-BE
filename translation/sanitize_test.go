86d2b0aa794f2ef221ee2c40f9b9221f8dd29803
Prushka <dan@lyu.sh>
2025-10-09 20:46:40 +0800

    Add process test

--- translation/sanitize_test.go
+++ translation/sanitize_test.go
@@ -2,6 +2,7 @@ package translation
 
 import (
 	"Sparkle/discord"
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
@@ -9,13 +10,11 @@ import (
 )
 
 func TestSanitization(t *testing.T) {
-	// read from output.vtt
 	fBytes, err := os.ReadFile("3-eng.ass")
 	if err != nil {
 		t.Fatalf("Failed to read file: %v", err)
 	}
 
-	// write to output_sanitized.vtt
 	sub, err := sanitizeInputASS(string(fBytes))
 	if err != nil {
 		t.Fatalf("Failed to sanitize input: %v", err)
@@ -31,6 +30,32 @@ func TestSanitization(t *testing.T) {
 		t.Fatalf("Failed to write sanitized file: %v", err)
 	}
 
+}
+
+func TestProcess(t *testing.T) {
+
+	fBytes, err := os.ReadFile("5-eng.ass")
+	if err != nil {
+		t.Fatalf("Failed to read file: %v", err)
+	}
+
+	sub, err := sanitizeInputASS(string(fBytes))
+	if err != nil {
+		t.Fatalf("Failed to sanitize input: %v", err)
+	}
+	inputsPairs := splitByCharacters(sub.distilledDialogues, 29000)
+	text, err := sub.process(inputsPairs[0], []string{
+		"0:0:03.34,0:00:04.43,From my room,",
+		"0:00:04.63,0:00:07.13,I can see the neighbor's backyard.",
+		"0:00:07.34,0:00:19.13,It doesn't get a lot of sunlight...",
+		"0:00:27.65,0:00:32.24,Rina-chan from next door is \na few years younger than I am,",
+		"0:00:32.45,0:00:44.04,but apparently she's been sick all her life",
+		"0:00:34.24,0:00:36.03,and never leaves the house.",
+	})
+	if err != nil {
+		t.Fatalf("Failed to process: %v", err)
+	}
+	fmt.Println(text)
 }
 
 func TestPrintMalformedASS(t *testing.T) {
